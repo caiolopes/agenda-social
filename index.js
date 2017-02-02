eb864037bf36ca11dfe227bbd52279b4d35baed0
Caio Lopes <caiodesalopes@gmail.com>
2017-02-02 18:07:39 -0200

    Mais coisas

--- index.js
+++ index.js
@@ -10,16 +10,36 @@ const bot = new BootBot({
 });
 
 
+bot.on('attachment', (payload, chat) => {});
+
+
 bot.hear(['Olá', 'Oi', /eai( there)?/i], (payload, chat) => {
     console.log('HELLO WORLD!');
-    chat.say({
-        text: 'Precisa de ajuda?!',
-        buttons: [
-            { type: 'postback', title: 'Sim', payload: 'HELP_SETTINGS' },
-            { type: 'postback', title: 'Não', payload: 'HELP_FAQ' },
-            { type: 'postback', title: 'Talvez mais tarde', payload: 'HELP_HUMAN' }
-        ]
-    }, { typing: true });
+    chat.getUserProfile().then((user) => {
+        chat.say({
+            text: `Olá ${user.first_name}, gostaria de saber o que esta rolando na sua região?`,
+            buttons: [
+                { type: 'postback', title: 'Sim', payload: 'INICIO_SIM' },
+                { type: 'postback', title: 'Não', payload: 'INICIO_NAO' },
+                { type: 'postback', title: 'Talvez mais tarde', payload: 'INICIO_TALVEZ' }
+            ]
+        }, { typing: true });
+    });
 });
+
+bot.on('postback:INICIO_SIM', (payload, chat) => {
+    chat.getUserProfile().then((user) => {
+        chat.getUserProfile().then((user) => {
+            chat.say({
+                text: 'Qual tipo de evento?',
+                buttons: [
+                    { type: 'postback', title: 'ONG\'s', payload: 'TIPO_ONG' },
+                    { type: 'postback', title: 'Eventos', payload: 'TIPO_EVENTOS' }
+                ]
+            }, { typing: true });
+        });
+    });
+});
+
 
 bot.start(8080);
